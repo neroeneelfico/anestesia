8b5e9b767aa680d707b39cd7a8a221cf3dd63164
Marcello Vecchio <marcello.vecchio92@gmail.com>
2021-09-21 02:09:58 +0200

    Convenzioni

--- frontend/views/site/profilo.php
+++ frontend/views/site/profilo.php
@@ -27,8 +27,20 @@ $this->params['breadcrumbs'][] = $this->title;
     <br><br>
     <?php
     if($model->convenzioni_id != ""){
+        $datainserita = new DateTime($model->convenzioni_tempo);
+        $dataattuale = date('Y-m-d H:i:s');
+        $dataattuale = new DateTime($dataattuale);
+        $since_start = $dataattuale->diff($datainserita);
+        $minutes = 0;
+        $minutes = $since_start->days * 24 * 60;
+        $minutes += $since_start->h * 60;
+        $minutes += $since_start->i;
         $convenzione = Convenzioni::find()->where(['id'=>$model->convenzioni_id])->one();
-        echo "La convenzione da te selezionata è: <b>".$convenzione->titolo."</b>: ".$convenzione->citta;
+        echo "La convenzione da te selezionata è: <b>".$convenzione->titolo."</b>: ".$convenzione->citta."<br><br>";
+        if($minutes < 360)
+        echo "Potrai cambiare la tua scelta tra ".(360 -$minutes)." minuti";
+        else
+            echo "Se vuoi, puoi già effettuare un cambio della convenzione accedendo al menu Convenzioni";
     }
     else {
         echo "Non hai ancora selezionato nessuna convenzione. Accedi nella sezione <b>Convenzioni</b> per procedere";
